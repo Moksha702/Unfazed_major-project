6debd37404c96c538b224d95f403c55c62a90a11
Moksha702 <mokshasai3@gmail.com>
2026-09-25 00:18:47 +0530

    update

--- unfazed-backend/server.js
+++ unfazed-backend/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const http = require('http');
+const cors = require('cors');
 const { Server } = require('socket.io');
 const app = require('./src/app');
 const connectDB = require('./src/config/db');
